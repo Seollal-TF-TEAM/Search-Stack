70be59ba42a301eb69b6d43087b788685d4c1258
Seulg2027 <seulgi2027@dgu.ac.kr>
2025-01-29 07:20:29 +0900

    [Feat] Add Logging program

--- src/main/java/com/example/searchstack/controller/SearchController.java
+++ src/main/java/com/example/searchstack/controller/SearchController.java
@@ -1,5 +1,9 @@
 package com.example.searchstack.controller;
 
+import com.example.searchstack.config.exception.ApiException;
+import com.example.searchstack.config.exception.ErrorCode;
+import com.example.searchstack.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -7,8 +11,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/search")
 public class SearchController {
-    @PostMapping("/new")
-    public ResponseEntity<String> newSearchEnter(@RequestParam @Validated String key) {
+    private final SearchService searchService;
+
+    @Autowired
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping("/new")
+    public ResponseEntity<String> newSearchEnter(@RequestParam @Validated String query) {
+        if (query.equals("") || query == null) {
+            throw new ApiException(ErrorCode.BAD_REQUEST_ERROR);
+        }
+        searchService.search(query);
 
         return ResponseEntity.ok("ok");
     }
--- src/main/java/com/example/searchstack/service/SearchService.java
+++ src/main/java/com/example/searchstack/service/SearchService.java
@@ -1,5 +1,27 @@
 package com.example.searchstack.service;
 
-public class SearchService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+
+@Service
+public class SearchService {
+    private static final Logger logger = LoggerFactory.getLogger(SearchService.class);
+
+    public void search(String query) {
+        logger.debug("Search query: {}", query);
+    }
+
+    public void logToLogstash(String keyword) {
+        String jsonLog = "{\"keyword\": " + keyword + "}";
+        try (Socket socket = new Socket("localhost", 5044);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+            out.println(jsonLog);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
