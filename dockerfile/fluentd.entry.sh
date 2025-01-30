d978031f8b6643f6957780ffee98f412ee334665
dyoun12 <gdaeyeon002@gmail.com>
2025-01-30 11:46:08 +0900

    [REFACTOR] App system log 형식 수정

--- dockerfile/fluentd.entry.sh
+++ dockerfile/fluentd.entry.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # 로그 파일이 없으면 생성
-touch /var/log/test.log
+touch /var/log/test_sys.log
 # fluent 유저가 파일을 사용할 수 있도록 변경
-chown fluent:fluent /var/log/test.log
-chmod 644 /var/log/test.log
+chown fluent:fluent /var/log/test_sys.log
+chmod 644 /var/log/test_sys.log
+
+touch /var/log/test_user.log
+# fluent 유저가 파일을 사용할 수 있도록 변경
+chown fluent:fluent /var/log/test_user.log
+chmod 644 /var/log/test_user.log
 # Fluentd 실행
 exec fluentd -c /fluentd/etc/fluent.conf -p /fluentd/plugins
